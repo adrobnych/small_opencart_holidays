5c611e8bee1f5675ca84efd0ff9145bb695c32e1
Erik <erikpolachek@gmail.com>
2015-05-06 10:08:11 +0300

    new opencart project

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/drobnych/admin/');
+define('HTTP_CATALOG', 'http://localhost/drobnych/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/drobnych/admin/');
+define('HTTPS_CATALOG', 'http://localhost/drobnych/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/drobnych/admin/');
+define('DIR_SYSTEM', '/var/www/drobnych/system/');
+define('DIR_LANGUAGE', '/var/www/drobnych/admin/language/');
+define('DIR_TEMPLATE', '/var/www/drobnych/admin/view/template/');
+define('DIR_CONFIG', '/var/www/drobnych/system/config/');
+define('DIR_IMAGE', '/var/www/drobnych/image/');
+define('DIR_CACHE', '/var/www/drobnych/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/drobnych/system/download/');
+define('DIR_UPLOAD', '/var/www/drobnych/system/upload/');
+define('DIR_LOGS', '/var/www/drobnych/system/logs/');
+define('DIR_MODIFICATION', '/var/www/drobnych/system/modification/');
+define('DIR_CATALOG', '/var/www/drobnych/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'sparco1012');
+define('DB_DATABASE', 'drobnych_opencart');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/drobnych/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/drobnych/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/drobnych/catalog/');
+define('DIR_SYSTEM', '/var/www/drobnych/system/');
+define('DIR_LANGUAGE', '/var/www/drobnych/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/drobnych/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/drobnych/system/config/');
+define('DIR_IMAGE', '/var/www/drobnych/image/');
+define('DIR_CACHE', '/var/www/drobnych/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/drobnych/system/download/');
+define('DIR_UPLOAD', '/var/www/drobnych/system/upload/');
+define('DIR_MODIFICATION', '/var/www/drobnych/system/modification/');
+define('DIR_LOGS', '/var/www/drobnych/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'sparco1012');
+define('DB_DATABASE', 'drobnych_opencart');
+define('DB_PREFIX', 'oc_');
